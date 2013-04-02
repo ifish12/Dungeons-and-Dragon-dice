fad5eb75bb10a48b364098c68ebc3e9ea5f9e278
Geoff Shapiro <gshapiro49@gmail.com>
2013-04-02 01:51:47 -0400

    So I got bored and decided I needed this so I wrote it.
    It's really simple but a lot of fun.

---
+++ Dungeons and Dragon dice/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Dungeons_and_Dragon_dice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnD4 = new System.Windows.Forms.Button();
+            this.btnD6 = new System.Windows.Forms.Button();
+            this.btnD20 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnD10 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnD12 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnD8 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnD4
+            // 
+            this.btnD4.Location = new System.Drawing.Point(13, 13);
+            this.btnD4.Name = "btnD4";
+            this.btnD4.Size = new System.Drawing.Size(75, 23);
+            this.btnD4.TabIndex = 0;
+            this.btnD4.Text = "d4";
+            this.btnD4.UseVisualStyleBackColor = true;
+            this.btnD4.Click += new System.EventHandler(this.btnD4_Click);
+            // 
+            // btnD6
+            // 
+            this.btnD6.Location = new System.Drawing.Point(13, 42);
+            this.btnD6.Name = "btnD6";
+            this.btnD6.Size = new System.Drawing.Size(75, 23);
+            this.btnD6.TabIndex = 1;
+            this.btnD6.Text = "d6";
+            this.btnD6.UseVisualStyleBackColor = true;
+            this.btnD6.Click += new System.EventHandler(this.btnD6_Click);
+            // 
+            // btnD20
+            // 
+            this.btnD20.Location = new System.Drawing.Point(14, 158);
+            this.btnD20.Name = "btnD20";
+            this.btnD20.Size = new System.Drawing.Size(75, 23);
+            this.btnD20.TabIndex = 2;
+            this.btnD20.Text = "d20";
+            this.btnD20.UseVisualStyleBackColor = true;
+            this.btnD20.Click += new System.EventHandler(this.btnD20_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(126, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 23);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(126, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 23);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(127, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 23);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(126, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 23);
+            this.label4.TabIndex = 7;
+            // 
+            // btnD10
+            // 
+            this.btnD10.Location = new System.Drawing.Point(13, 100);
+            this.btnD10.Name = "btnD10";
+            this.btnD10.Size = new System.Drawing.Size(75, 23);
+            this.btnD10.TabIndex = 6;
+            this.btnD10.Text = "d10";
+            this.btnD10.UseVisualStyleBackColor = true;
+            this.btnD10.Click += new System.EventHandler(this.btnD10_Click);
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(126, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 23);
+            this.label5.TabIndex = 9;
+            // 
+            // btnD12
+            // 
+            this.btnD12.Location = new System.Drawing.Point(13, 129);
+            this.btnD12.Name = "btnD12";
+            this.btnD12.Size = new System.Drawing.Size(75, 23);
+            this.btnD12.TabIndex = 8;
+            this.btnD12.Text = "d12";
+            this.btnD12.UseVisualStyleBackColor = true;
+            this.btnD12.Click += new System.EventHandler(this.btnD12_Click);
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(126, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 23);
+            this.label6.TabIndex = 11;
+            // 
+            // btnD8
+            // 
+            this.btnD8.Location = new System.Drawing.Point(13, 71);
+            this.btnD8.Name = "btnD8";
+            this.btnD8.Size = new System.Drawing.Size(75, 23);
+            this.btnD8.TabIndex = 10;
+            this.btnD8.Text = "d8";
+            this.btnD8.UseVisualStyleBackColor = true;
+            this.btnD8.Click += new System.EventHandler(this.btnD8_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.btnGen);
+            this.groupBox1.Controls.Add(this.txt2);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txt1);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Location = new System.Drawing.Point(14, 188);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(198, 100);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "dX";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Value 1";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(92, 13);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(92, 39);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Value 2";
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(7, 71);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 23);
+            this.btnGen.TabIndex = 4;
+            this.btnGen.Text = "Generate";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label9.Location = new System.Drawing.Point(127, 71);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 23);
+            this.label9.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 312);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnD8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnD12);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnD10);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnD20);
+            this.Controls.Add(this.btnD6);
+            this.Controls.Add(this.btnD4);
+            this.Name = "Form1";
+            this.Text = "Dice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnD4;
+        private System.Windows.Forms.Button btnD6;
+        private System.Windows.Forms.Button btnD20;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnD10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnD12;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnD8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Dungeons and Dragon dice/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dungeons_and_Dragon_dice
+{
+    public partial class Form1 : Form
+    {
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnD4_Click(object sender, EventArgs e)
+        {
+            Random D4Rand = new Random();
+            int D4 = D4Rand.Next(1, 5);
+
+            label1.Text = D4.ToString();
+        }
+
+        private void btnD6_Click(object sender, EventArgs e)
+        {
+            Random D6Rand = new Random();
+            int D6 = D6Rand.Next(1, 7);
+
+            label2.Text = D6.ToString();
+        }
+
+        private void btnD20_Click(object sender, EventArgs e)
+        {
+            Random D20Rand = new Random();
+            int D20 = D20Rand.Next(1, 21);
+
+            label3.Text = D20.ToString();
+        }
+
+        private void btnD10_Click(object sender, EventArgs e)
+        {
+            Random D10Rand = new Random();
+            int D10 = D10Rand.Next(1, 11);
+
+            label4.Text = D10.ToString();
+        }
+
+        private void btnD12_Click(object sender, EventArgs e)
+        {
+            Random D12Rand = new Random();
+            int D12 = D12Rand.Next(1, 13);
+
+            label5.Text = D12.ToString();
+        }
+
+        
+        
+
+        private void btnD8_Click(object sender, EventArgs e)
+        {
+            Random D8Rand = new Random();
+            int D8 = D8Rand.Next(1, 9);
+
+            label6.Text = D8.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            int val1 = 0;
+            int val2 = 0;
+
+            val1 = int.Parse(txt1.Text);
+            val2 = int.Parse(txt2.Text);
+
+            Random DXRand = new Random();
+            int DX = DXRand.Next(val1, val2+1);
+
+            label9.Text = DX.ToString();
+        }
+    }
+}
